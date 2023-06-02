a3f7a4448bcdce04e6697be5db58bac24b15e211
egmprog <egmprog@gmail.com>
2023-06-01 22:56:42 -0300

    20230531

--- ClasesCaC/Operacion.java
+++ ClasesCaC/Operacion.java
@@ -1,51 +1,44 @@
 package ClasesCaC;
 
 public class Operacion {
-    //atributos
+    // atributos
 
-private int num1;
-private int num2;
-private int operador;
-private int resultado;
+    private int num1;
+    private int num2;
+    private int operador;
+    private int resultado;
 
-
-public void sumar (int num1, int num2){
-    this.resultado=num1+num2;
-}
-
-public void restar (int num1, int num2){
-    this.resultado=num1-num2;
-}
-
-public void multiplicar (int num1, int num2){
-    this.resultado=num1*num2;
-}
-
-public void dividir (int num1, int num2){
-    if (num2!=0){
-        this.resultado=num1/num2;
-    }else{
-        this.resultado=0;
+    public void sumar(int num1, int num2) {
+        this.resultado = num1 + num2;
     }
 
+    public void restar(int num1, int num2) {
+        this.resultado = num1 - num2;
+    }
 
-    
+    public void multiplicar(int num1, int num2) {
+        this.resultado = num1 * num2;
+    }
 
+    public void dividir(int num1, int num2) {
+        if (num2 != 0) {
+            this.resultado = num1 / num2;
+        } else {
+            this.resultado = 0;
+        }
 
-}
-
+    }
 
     public Operacion(int num1, int num2, int operador, int resultado) {
         this.num1 = num1;
         this.num2 = num2;
         this.operador = operador;
-        
+
     }
 
     @Override
     public String toString() {
-        return "Operacion{"+num1+" , "+num2+" , "+operador+" , "+resultado;
+        return "{" + num1 + " , " + num2 + " , " + operador + " , " + resultado + "}";
     }
-    
 
 }
--- ClasesCaC/introd12.java
+++ ClasesCaC/introd12.java
@@ -6,70 +6,68 @@ import java.util.Scanner;
 /**
  * Comentarios de documentación
  * Título del programa
+ * 
  * @autor: Eduardo Molino
  * @version 1.0
  */
 
-//Temas desarrollados en la clase del 30/05/2023
+// Temas desarrollados en la clase del 30/05/2023
 
 /*
  * Temas:
  * ArrayList
- * Clases (se usa una clase creada en archivo aparte llamada: Auto)
+ * Clases (se usa una clase creada en archivo aparte llamada: Operacion)
  */
 
-
-
-
-public class introd12
-{
-    public static void main(String[] args)
-    {
+public class introd12 {
+    public static void main(String[] args) {
         System.out.println("---------------------INICIO--------------------");
-        Scanner teclado = new Scanner (System.in);
+        Scanner teclado = new Scanner(System.in);
         ArrayList<Operacion> listaOperaciones = new ArrayList<Operacion>();
-        int num1,num2,operador;
+        int num1, num2, operador;
 
-        for (int i=1; i<5; i++){
+        for (int i = 1; i < 5; i++) {
             System.out.println("ingrese el oprador");
             System.out.println("1. sumar, 2. restar, 3. multiplicar, 4. dividir");
-            operador=teclado.nextInt();
+            operador = teclado.nextInt();
             System.out.println("Ingrese el numero 1:");
-            num1=teclado.nextInt();
+            num1 = teclado.nextInt();
             System.out.println("Ingrese el numero 2:");
-            num2=teclado.nextInt();
+            num2 = teclado.nextInt();
 
-            //creo el objeto operacion
-            Operacion miOperacion= new Operacion(num1, num2, operador, i);
+            // creo el objeto operacion
+            Operacion miOperacion = new Operacion(num1, num2, operador, i);
 
             switch (operador) {
 
-                case 1:miOperacion.sumar(num1,num2);break;
-                case 2:miOperacion.restar(num1,num2);break;
-                case 3:miOperacion.multiplicar(num1,num2);break;
-                case 4:miOperacion.dividir(num1,num2);break;
-                    
-                    
-            
+                case 1:
+                    miOperacion.sumar(num1, num2);
+                    break;
+                case 2:
+                    miOperacion.restar(num1, num2);
+                    break;
+                case 3:
+                    miOperacion.multiplicar(num1, num2);
+                    break;
+                case 4:
+                    miOperacion.dividir(num1, num2);
+                    break;
+
                 default:
-                System.out.println("Operador inválido");
+                    System.out.println("Operador inválido");
                     break;
             }
 
-            if(operador==1||operador==2||operador==3||operador==4){
+            if (operador == 1 || operador == 2 || operador == 3 || operador == 4) {
                 listaOperaciones.add(miOperacion);
             }
 
-
-
         }
 
-        
         System.out.println(listaOperaciones);
-        
-        System.out.println("---------------------FIN--------------------");
-    teclado.close();    
-    }
-    
-}
 
+        System.out.println("---------------------FIN--------------------");
+        teclado.close();
+    }
+
+}
